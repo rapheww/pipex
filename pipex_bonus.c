e34e14b2c9f2d0c935843c66db3b93e1fbcb8247
Raphael Chaumeil <rchaumei@f4r1s8.clusters.42paris.fr>
2026-01-31 00:15:41 +0100

    push bonus 1

--- pipex_bonus.c
+++ pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rchaumei <rchaumei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 20:55:20 by rchaumei          #+#    #+#             */
-/*   Updated: 2026/01/28 23:31:58 by rchaumei         ###   ########.fr       */
+/*   Updated: 2026/01/31 00:12:31 by rchaumei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ char	**get_env(char **env)
 {
 	char	**path;
 
-	while (env)
+	while (*env)
 	{
 		if (ft_strncmp(*env, "PATH", 4) == 0)
 			break ;
@@ -44,16 +44,100 @@ char	**get_env(char **env)
 	return (path);
 }
 
+char	*get_cmd_path(char *cmd, char **path)
+{
+	char	*command;
+	char	*tmp;
+	int		i;
+
+	i = 0;
+	if (access(cmd, X_OK) == 0)
+		return (cmd);
+	while (path[i])
+	{
+		command = ft_strjoin(path[i], "/");
+		if (!command)
+			return (NULL);
+		tmp = ft_strjoin(command, cmd);
+		free(command);
+		if (!tmp)
+			return (NULL);
+		if (access(tmp, X_OK) == 0)
+			return (tmp);
+		free(tmp);
+		i++;
+	}
+	return (ft_putstr_fd("Invalid command\n", 2), NULL);
+}
+
+void child(char **envp, char **av, int i, int pid, char **path, int fd_in, int fd_out)
+{
+	char **cmd;
+	char *cmd_path;
+	
+	cmd = ft_split(av[i + 2], ' ');
+	cmd_path = get_cmd_path(cmd[0], path);
+	dup2(fd_out, 1);
+	dup2(fd_in, 0);
+	close(fd_in);
+	close(fd_out);
+	execve(cmd_path, cmd, envp);
+}
+
 int main(int ac, char **av, char **envp)
 {
     int in;
     int out;
-    char **env;
+    char **path;
+	int i;
+	int status;
+	int *pid;
+	char **cmd;
+	int fds[2];
+	int fd_in;
+
     if (ac >= 5)
     {
         in = check_infile(av[1]);
         out = check_outfile(av[ac - 1]);
-        env = get_env(char **env);
+        path = get_env(envp);
+		i = 0;
+		pid = malloc(sizeof(int) * (ac - 3));
+		fd_in = in;
+		while (i  < ac - 4)
+		{
+			pipe(fds);
+			pid[i] = fork();
+			if (!pid[i])
+			{
+				close(fds[0]);
+				child(envp, av, i, pid[i], path, fd_in, fds[1]);
+			}
+			close(fd_in);
+			close(fds[1]);
+			fd_in = dup(fds[0]);
+			close(fds[0]);
+			i++;
+				
+		}
+		pid[i] = fork();
+		if (pid[i] == 0)
+		{
+			dup2(fd_in, 0);
+			dup2(out, 1);
+			close(fd_in);
+			close(out);
+			cmd = ft_split(av[ac - 2], ' ');
+			execve(get_cmd_path(cmd[0], path), cmd, envp);
+		}
+		close(fd_in);
+		close(out);
+		i = 0;
+		while(i < ac - 3)
+		{
+			waitpid(pid[i], &status, 0);
+			i++;
+		}
     }
     return (0);
 }
